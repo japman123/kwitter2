21a5044e3ca7f981b0d50a373c37bb290281b7de
japman123 <87320271+japman123@users.noreply.github.com>
2022-05-06 22:07:49 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAG6ZOOYEKL46pOSYl8XwBvajcoBY3_bso",
+    authDomain: "kwitterproject-8aa81.firebaseapp.com",
+    databaseURL: "https://kwitterproject-8aa81-default-rtdb.firebaseio.com",
+    projectId: "kwitterproject-8aa81",
+    storageBucket: "kwitterproject-8aa81.appspot.com",
+    messagingSenderId: "257268451210",
+    appId: "1:257268451210:web:470976ab6fa16208ce2a73"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
